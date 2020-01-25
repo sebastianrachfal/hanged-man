edf5e9fc74ada4f74db44470646dcbcc0a64f163
sebastianrachfal <rachfalsebastian@gmail.com>
2020-01-26 00:30:16 +0100

    Updates

---
+++ precache-manifest.94985a43be4c71c8f6313203809455d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa186bdd63887c616dc14a38ecd61297",
+    "url": "sebastianrachfal.github.io/hanged-man/index.html"
+  },
+  {
+    "revision": "22545d8953dd2d0eb2cf",
+    "url": "sebastianrachfal.github.io/hanged-man/static/css/main.a1f38011.chunk.css"
+  },
+  {
+    "revision": "725f6ac8c98b6dab2e86",
+    "url": "sebastianrachfal.github.io/hanged-man/static/js/2.a08938c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "sebastianrachfal.github.io/hanged-man/static/js/2.a08938c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22545d8953dd2d0eb2cf",
+    "url": "sebastianrachfal.github.io/hanged-man/static/js/main.44883065.chunk.js"
+  },
+  {
+    "revision": "f60dfd668e7e41216091",
+    "url": "sebastianrachfal.github.io/hanged-man/static/js/runtime-main.36f66594.js"
+  }
+]);
